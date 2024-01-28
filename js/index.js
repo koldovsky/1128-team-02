795473d17adcf62e443cc811c24fb5cb69d22db5
ZenukD <148074336+ZenukD@users.noreply.github.com>
2024-01-28 22:30:39 +0000

    add js

--- js/index.js
+++ js/index.js
@@ -3,4 +3,5 @@ document.addEventListener("partialsLoaded", () => {
     import('./trending-products-carousel.js');
     import('./products-carousel.js');
     import('./burger-menu.js');
+    import('./reviews-carousel.js');
 });
