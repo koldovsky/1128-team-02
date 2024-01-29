4c6a2a21182e13da4aa9799022a9ede7a83e803b
Zerkampf85 <66432379+Zerkampf85@users.noreply.github.com>
2024-01-29 17:43:00 +0000

    js

--- js/products-carousel.js
+++ js/products-carousel.js
@@ -1,32 +1,40 @@
-let currentSlideIndex = 0;
-const slides = document.querySelectorAll('.products__carousel-animals-item');
-const totalSlides = slides.length;
+const slides = [
+];
 
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        if (i === index) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
+let slideIdx = 0;
+
+const slideContainer = document.querySelector('.products__carousel-animals-item');
+
+function renderSlide() {
+    slideContainer.innerHTML = slides[slideIdx];
+    if (window.matchMedia('(min-width: 768px)').matches ) {
+        const secondSlideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
+        slideContainer.innerHTML += slides[secondSlideIdx];
+        if (window.matchMedia('(min-width: 980px)').matches) {
+            const thirdSlideIdx = secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
+            slideContainer.innerHTML += slides[thirdSlideIdx];
         }
-    });
+    }
 }
 
 function nextSlide() {
-    currentSlideIndex++;
-    if (currentSlideIndex >= totalSlides) {
-        currentSlideIndex = 0;
-    }
-    showSlide(currentSlideIndex);
+    slideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
+    renderSlide();
 }
 
 function prevSlide() {
-    currentSlideIndex--;
-    if (currentSlideIndex < 0) {
-        currentSlideIndex = totalSlides - 1;
-    }
-    showSlide(currentSlideIndex);
+    slideIdx = slideIdx - 1 < 0 ? slides.length - 1 : slideIdx - 1;
+    renderSlide();
 }
 
-// Показуємо перший слайд початково
-showSlide(currentSlideIndex);
+setInterval(nextSlide, 3000);
+
+const btnNext = document.querySelector('.next');
+btnNext.addEventListener('click', nextSlide);
+
+const btnPrev = document.querySelector('.prev');
+btnPrev.addEventListener('click', prevSlide);
+
+renderSlide();
+
+window.addEventListener('resize', renderSlide);
