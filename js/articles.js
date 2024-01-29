9ba7283283f58ef7f1413c0d24f9eadcfe279398
Volodymyr Bilokon <bilokonvolodymyr@gmail.com>
2024-01-29 10:44:11 +0200

    add js

---
+++ js/articles.js
@@ -0,0 +1,15 @@
+function secretText(page) {
+    const articlesTitle = document.querySelector(".articles__title");
+    articlesTitle.innerText = "First Project";
+  }
+  function originalText(page) {
+    const articlesTitle = document.querySelector(".articles__title");
+    articlesTitle.innerText = "Our News & Articles";
+  }
+  function bigLink(page) {
+    page.style.fontSize = "50px";
+  }
+  function smallLink(page) {
+    page.style.fontSize = "15px";
+  }
+  
