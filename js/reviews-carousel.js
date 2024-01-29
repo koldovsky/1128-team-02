479528486979cf9de7e489c7607c52cf5275fe5b
ZenukD <148074336+ZenukD@users.noreply.github.com>
2024-01-29 00:31:33 +0000

    carousel repair

--- js/reviews-carousel.js
+++ js/reviews-carousel.js
@@ -1,44 +1,54 @@
-const carouselReview = document.querySelector('.customer-reviews__carousel-slides');
-const slidesReview = document.querySelector('.customer-reviews__carousel-slides-item');
-const prevButtonReview = document.querySelector('.customer-reviews__carousel-btn-prev');
-const nextButtonReview = document.querySelector('.customer-reviews__carousel-btn-next');
+const carouselReview = document.querySelector(
+  ".customer-reviews__carousel-slides"
+);
+const slidesReview = document.querySelectorAll(
+  ".customer-reviews__carousel-slides-item"
+);
+const prevButtonReview = document.querySelector(
+  ".customer-reviews__carousel-btn-prev"
+);
+const nextButtonReview = document.querySelector(
+  ".customer-reviews__carousel-btn-next"
+);
 
 let currentIndexReview = 0;
-let carouselWidthReview = carouselReview.offsetWidth;
+let carouselReviewWidthReview = carouselReview.offsetWidth;
 
 showSlideReview(currentIndexReview);
 
-function showSlideReview(index){
-    carouselReview.style.transform = `translateX(-${index * (100 / getVisibleSlides())}%)`;
+function showSlideReview(index) {
+  carouselReview.style.transform = `translateX(-${
+    index * (100 / getVisibleSlidesReview())
+  }%)`;
 }
 
 function nextSlideReview() {
-    if(currentIndexReview < slidesReview.length - getVisibleSlidesReview()) {
-        currentIndexReview++;
-        showSlideReview(currentIndexReview);
-    }
+  if (currentIndexReview < slidesReview.length - getVisibleSlidesReview()) {
+    currentIndexReview++;
+    showSlideReview(currentIndexReview);
+  }
 }
 
 function prevSlideReview() {
-    if(currentIndexReview > 0) {
-        currentIndexReview--;
-        showSlideReview(currentIndexReview);
-    }
+  if (currentIndexReview > 0) {
+    currentIndexReview--;
+    showSlideReview(currentIndexReview);
+  }
 }
 
-prevButtonReview.addEventListener('click', prevSlideReview);
-nextButtonReview.addEventListener('click', nextSlideReview);
+prevButtonReview.addEventListener("click", prevSlideReview);
+nextButtonReview.addEventListener("click", nextSlideReview);
 
 function getVisibleSlidesReview() {
-    if(window.innerWidth >= 1024) {
-        return 3;
-    }else if(window.innerWidth >= 768) {
-        return 2;
-    }else {
-        return 1;
-    }
+  if (window.innerWidth >= 1024) {
+    return 3;
+  } else if (window.innerWidth >= 768) {
+    return 2;
+  } else {
+    return 1;
+  }
 }
 
-window.addEventListener('resize', () => {
-    carouselWidthReview = carouselReview.offsetWidth;
+window.addEventListener("resize", () => {
+  carouselReviewWidthReview = carouselReview.offsetWidth;
 });
