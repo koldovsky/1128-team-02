ada7a77787435ca5ce11179e21a6f377d7a4f9ef
vetlitttz <130660203+vetlittz@users.noreply.github.com>
2024-01-30 14:50:57 +0000

    trying to fix my function on click

--- js/drytreats.js
+++ js/drytreats.js
@@ -1,14 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function toggleContent(category) {
-        var content = document.querySelector('.treats-category--' + category + ' .grid__container');
-        content.style.display = (content.style.display === 'none' || content.style.display === '') ? 'grid' : 'none';
-    }
-    var headers = document.querySelectorAll('.category-header');
+function toggleContent(category) {
+    const allCategories = document.querySelector('.treats__category');
+    allCategories.forEach( category => category.style.display = 'none');
+    var content = document.querySelector('.treats-category--' + category);
+    content.style.display = 'grid';
+}
+var headers = document.querySelectorAll('.category-header');
 
-    headers.forEach(function(header) {
-        header.addEventListener('click', function() {
-            var category = header.dataset.category;
-            toggleContent(category);
-        });
+headers.forEach(function(header) {
+    header.addEventListener('click', function() {
+        var category = header.dataset.category;
+        toggleContent(category);
     });
 });
+
