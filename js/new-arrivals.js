0c12a028b713ad9c5a8f82a487960ff10e7e259d
OlhaHubarieva <105358315+OlhaHubarieva@users.noreply.github.com>
2024-02-07 15:14:57 +0000

    js changes

--- js/new-arrivals.js
+++ js/new-arrivals.js
@@ -1,43 +1,96 @@
- //const newArrivals = [
-    {
-    id: "1",
-     link: "storehomepage.html",
-    image: "img/vitalifecrunchstickstreat.png",
-    alt: "VITALIFE Crunch Sticks Treat",
-    title: "VITALIFE Crunch Sticks Treat",
-    price: "$ 5,00",
-    },
-      {
-    id: "2",
-     link: "storehomepage.html",
-    image: "img/dunawoventoy.png",
-    alt: "DUNA Woven Toy",
-    title: "DUNA Woven Toy",
-    price: "$ 1,50",
-    },
-     {
-    id: "3",
-     link: "storehomepage.html",
-    image: "img/serawovenleash.png",
-    alt: "SERA Woven Leash",
-    title: "SERA Woven Leash",
-    price: "$ 2,00",
-    },
-    {
-    id: "4",
-     link: "storehomepage.html",
-    image: "img/optyplasticbowl.png",
-    alt: "OPTY Plastic Bowl",
-    title: "OPTY Plastic Bowl",
-    price: "$ 3,00",
-    },
-    {
-    id: "5",
-     link: "storehomepage.html",
-    image: "img/egobutterflytoy.png",
-    alt: "EGO Butterfly Toy,
-    title: "EGO Butterfly Toy",
-    price: "$ 8,00",
-    },
-]
+const newArrivals = [
+  `<figure class="arrivals__carousel-slides-item">
+  <img src = "img/dunawoventoy.png" alt = "DUNA Woven Toy" class="arrivals__carousel-slides-item-image"/>
+  <a href="storehomepage.html"><figcaption class="arrivals__carousel-slides-item-name">DUNA Woven Toy</figcaption>
+        </a>
+        <figcaption class="arrivals__carousel-slides-item-price">
+          $ 1,50
+        </figcaption>
+        <a href="storehomepage.html"
+          ><button class="arrivals__carousel-slides-item-btn-add" type="button">
+            <span class="arrivals__carousel-btn-content">Add to Cart</span>
+          </button></a
+        >
+      </figure >`,
 
+  `<figure class="arrivals__carousel-slides-item">
+  < img
+src = "img/serawovenleash.png"
+alt = "SERA Woven Leash"
+class="arrivals__carousel-slides-item-image"
+  />
+        <a href="storehomepage.html"
+          ><figcaption class="arrivals__carousel-slides-item-name">
+            SERA Woven Leash
+          </figcaption></a
+        >
+        <figcaption class="arrivals__carousel-slides-item-price">
+          $ 2,00
+        </figcaption>
+        <a href="storehomepage.html"
+          ><button class="arrivals__carousel-slides-item-btn-add" type="button">
+            <span class="arrivals__carousel-btn-content">Add to Cart</span>
+          </button></a
+        >
+      </figure >`,
+
+  `<figure class="arrivals__carousel-slides-item">
+  < img
+src = "img/optyplasticbowl.png"
+alt = "OPTY Plastic Bowl"
+class="arrivals__carousel-slides-item-image"
+  />
+        <a href="storehomepage.html"
+          ><figcaption class="arrivals__carousel-slides-item-name">
+            OPTY Plastic Bowl
+          </figcaption></a
+        >
+        <figcaption class="arrivals__carousel-slides-item-price">
+          $ 3,00
+        </figcaption>
+        <a href="storehomepage.html"
+          ><button class="arrivals__carousel-slides-item-btn-add" type="button">
+            <span class="arrivals__carousel-btn-content">Add to Cart</span>
+          </button></a
+        >
+      </figure >`,
+
+  `<figure class="arrivals__carousel-slides-item">
+  < img
+width = "480"
+src = "img/egobutterflytoy.png"
+alt = "EGO Butterfly Toy"
+class="arrivals__carousel-slides-item-image"
+  />
+        <a href="storehomepage.html"
+          ><figcaption class="arrivals__carousel-slides-item-name">
+            EGO Butterfly Toy
+          </figcaption></a
+        >
+        <figcaption class="arrivals__carousel-slides-item-price">
+          $ 8,00
+        </figcaption>
+        <a href="storehomepage.html"
+          ><button class="arrivals__carousel-slides-item-btn-add" type="button">
+            <span class="arrivals__carousel-btn-content">Add to Cart</span>
+          </button></a
+        >
+      </figure >`
+];
+
+let newArrivalsIdx = 0;
+
+const newArrivalsContainer = document.querySelector('.arrivals__carousel-slides');
+
+function renderNewArrivals() {
+  newArrivalsContainer.innerHTML = newArrivals[newArrivalsIdx];
+}
+
+function nextNewArrival() {
+  newArrivalsIdx = newArrivalsIdx + 1 >= newArrivals.length ? 0 : newArrivalsIdx + 1;
+  renderNewArrivals();
+}
+
+setInterval(nextNewArrival, 3000);
+
+renderNewArrivals();
