f017438d4cf1c9d11bddc576fe1b38eef9131bed
Stanny <stannyb9@gmail.com>
2024-02-07 18:16:18 +0200

    new changes

--- js/cart-modal.js
+++ js/cart-modal.js
@@ -1,13 +1,13 @@
-const quantityInput = document.querySelector(".purchase__quantity-input-modal");
-const buttonUp = document.querySelector(".button-up-modal");
-const buttonDown = document.querySelector(".button-down-modal");
+// const quantityInput = document.querySelector(".purchase__quantity-input-modal");
+// const buttonUp = document.querySelector(".button-up-modal");
+// const buttonDown = document.querySelector(".button-down-modal");
 
-buttonUp.addEventListener("click", function () {
-  quantityInput.value = parseInt(quantityInput.value, 10) + 1;
-});
+// buttonUp.addEventListener("click", function () {
+//   quantityInput.value = parseInt(quantityInput.value, 10) + 1;
+// });
 
-buttonDown.addEventListener("click", function () {
-  if (quantityInput.value > 0) {
-    quantityInput.value = parseInt(quantityInput.value, 10) - 1;
-  }
-});
+// buttonDown.addEventListener("click", function () {
+//   if (quantityInput.value > 0) {
+//     quantityInput.value = parseInt(quantityInput.value, 10) - 1;
+//   }
+// });
